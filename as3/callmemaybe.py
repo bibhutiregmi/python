e016f5ee90361072beee88dad857db3b3954f331
Bibhuti <bibhuti@es.cloudfactory.com>
2016-10-29 13:19:48 +0545

    delete record added

--- as3/callmemaybe.py
+++ as3/callmemaybe.py
@@ -1,17 +1,19 @@
 telephone_dir = {"apple":9,"ball":10}
-
 basic_func = {1:"view",2:"insert",3:"delete",4:"search",5:"store in csv",6:"exit"}
 search_dir = {1:"by name",2:"by number"}
+
 def option_selection():
   choice = raw_input("Select the number: ")
   print 
   if (choice == "1"):
     view_op()
-  if (choice == "2"):
+  elif (choice == "2"):
     insert_op()
-  if (choice == "4"):
+  elif (choice == "3"):
+    delete_op()
+  elif (choice == "4"):
     search_op()
-  if (choice == "6"):
+  elif (choice == "6"):
     quit()
 
 def search_selection():
@@ -55,13 +57,26 @@ def display_record(no,name):
   print "*********************"
 
 def search_num():
-  search_no = raw_input("Select the number: ")
+  search_no = raw_input("Number to search: ")
   for name,no in telephone_dir.items():
     if (int(no) == int(search_no)):
       display_record(no,name)
+    else:
+      print "not found"
   basic_operation()
 
-
+def delete_op():
+  view_available_option()
+  name = raw_input("Name to delete: ")
+  if name in telephone_dir:
+    del telephone_dir[name]
+    print "Deleted safely"
+    print "\n"
+    print "****************"
+  else:
+    print "Not found"
+  view_op()
+  
 def search_name():
   name = raw_input("Name to search: ")
   if name in telephone_dir:
@@ -70,12 +85,15 @@ def search_name():
     print "not found"
   basic_operation()
 
-def view_op():
+def view_available_option():
   final_options = sorted(telephone_dir.items())
   print "Name\tPhone"
   for i in final_options:
     print "%s\t%s" %(i[0],i[1])
   print "*********************"
+
+def view_op():
+  view_available_option()
   basic_operation()
 
 def main():
